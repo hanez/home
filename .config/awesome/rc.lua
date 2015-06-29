62be86cff8507b4f5fcc24b674d1abff0f45a695
Johannes Findeisen <you@hanez.org>
2015-06-29 04:22:48 +0200

    just some cleanups

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -102,25 +102,21 @@ end
 -- }}}
 
 -- Initialize widgets
-
 local batwidget  = wibox.widget.textbox()
-vicious.register(batwidget, vicious.widgets.bat, "BAT: $1C | ", 20, "BAT0" )
+vicious.register(batwidget, vicious.widgets.bat, "$2%/$3H | ", 20, "BAT0" )
 
 local thermalwidget  = wibox.widget.textbox()
-vicious.register(thermalwidget, vicious.widgets.thermal, "CPU: $1C | ", 20, "thermal_zone0" )
+vicious.register(thermalwidget, vicious.widgets.thermal, "$1C | ", 20, "thermal_zone0" )
 
 cpuwidget = awful.widget.graph()
--- Graph properties
 cpuwidget:set_width(50)
 cpuwidget:set_background_color("#222222")
 cpuwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 10,0 }, stops = { {0, "#FF5656"}, {0.5, "#88A175"}, 
-                    {1, "#AECF96" }}})
-                     -- Register widget
-                    vicious.register(cpuwidget, vicious.widgets.cpu, "$1")
+ {1, "#AECF96" }}})
+ vicious.register(cpuwidget, vicious.widgets.cpu, "$1")
 
 -- {{{ Menu
 -- Create a laucher widget and a main menu
-
 accessoriesmenu = {
    { "calculator", "/usr/bin/galculator" },
 }
