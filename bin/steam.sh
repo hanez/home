e3feb62c5162ee592ac3acd37e92dbd88663a737
Johannes Findeisen <you@hanez.org>
2018-01-15 20:38:52 +0100

    added ~/bin to this repo and steam launcher helder script to mitigate some errors

---
+++ bin/steam.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#/usr/bin/steam &
+#cd /home/hanez/.local/share/Steam/steamapps/common/Thimbleweed\ Park
+#export LD_LIBRARY_PATH="./"
+
+TP_PATH="/home/hanez/.local/share/Steam/steamapps/common/Thimbleweed\ Park"
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_APTH:$TP_PATH
+
+/usr/bin/steam
