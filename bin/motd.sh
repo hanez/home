22815a6f9f516e93c8b6741ce0e3173080dac437
Johannes Findeisen <you@hanez.org>
2019-01-18 22:29:20 +0100

    added comment to motd.sh

--- bin/motd.sh
+++ bin/motd.sh
@@ -1,5 +1,8 @@
 #!/bin/ash
 
+# this one is creating my /etc/motd file via cron to get some network 
+# statistics when logging in to my servers.
+
 echo "" > /etc/motd
 hostname -f >> /etc/motd
 echo "" >> /etc/motd
