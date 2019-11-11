b99a70fdaedea46636eca1b23edb8260be419049
Johannes Findeisen <hanez@systemchaos.org>
2019-11-11 16:48:32 +0100

    mirror source workaround

--- bin/artix.sh
+++ bin/artix.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+rsync -auvPH --delete-after "rsync://mirrors.dotsrc.org/artix-linux/" /data/artix/
 
-rsync -auvPH --delete-after "rsync://orion.artixlinux.org:64873/" /data/artix/repos/
-rsync -auvPH --delete-after "rsync://download.artixlinux.org/iso/" /data/artix/iso/
+#rsync -auvPH --delete-after "rsync://orion.artixlinux.org:64873/" /data/artix/repos/
+#rsync -auvPH --delete-after "rsync://download.artixlinux.org/iso/" /data/artix/iso/
 
