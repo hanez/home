26c66d7d8dfd414196cddaf9425a783432dde286
Johannes Findeisen <you@hanez.org>
2020-07-06 01:00:19 +0200

    made the quake terminal size configurable

--- .config/awesome/lib/functions.lua
+++ .config/awesome/lib/functions.lua
@@ -19,15 +19,3 @@ function tablelength(T)
     return count
 end
 
-local function set_wallpaper(s)
-    -- Wallpaper
-    if beautiful.wallpaper then
-        local wallpaper = beautiful.wallpaper
-        -- If wallpaper is a function, call it with the screen
-        if type(wallpaper) == "function" then
-            wallpaper = wallpaper(s)
-        end
-        gears.wallpaper.maximized(wallpaper, s, false)
-        --gears.wallpaper.centered(wallpaper, s, true)
-    end
-end
