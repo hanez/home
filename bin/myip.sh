e55392d16820f62324f6275e722cfe9637870505
Johannes Findeisen <you@hanez.org>
2020-07-25 01:42:32 +0200

    added isp to output

--- bin/myip.sh
+++ bin/myip.sh
@@ -5,5 +5,13 @@
 # Works too but very slow (a lot more features available):
 # curl ifconfig.me
 
-wget -qO- http://ipecho.net/plain; echo
+#wget -qO- http://ipecho.net/plain; echo
+
+MY_IP="`wget -qO- http://ipecho.net/plain`"
+
+echo $MY_IP
+
+MY_PROVIDER="`whois $MY_IP | awk '/role/{getline; print}' | awk -F  ":" '{print $2}' | awk '{$1=$1;print}'`"
+
+echo $MY_PROVIDER
 
