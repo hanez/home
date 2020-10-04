ae81e234c0fbbb816db80ef98da8d619d6b3e835
Johannes Findeisen <you@hanez.org>
2020-10-05 01:27:20 +0200

    small improvement for my daily work with awesome

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -77,12 +77,12 @@ end
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
+    awful.layout.suit.tile,
+    awful.layout.suit.tile.left,
     awful.layout.suit.fair,
     --awful.layout.suit.fair.horizontal,
     lain.layout.centerwork,
     --lain.layout.centerwork.horizontal,
-    awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
     --awful.layout.suit.tile.bottom,
     --awful.layout.suit.tile.top,
     --awful.layout.suit.spiral,
