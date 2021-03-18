690205156a221f1cb4eec6a83c8283cddaf4c5a8
Johannes Findeisen <you@hanez.org>
2021-03-18 06:21:05 +0100

    awesome rc.lua update

--- .config/awesome/jupiter_taskbar.lua
+++ .config/awesome/jupiter_taskbar.lua
@@ -48,7 +48,7 @@ awful.screen.connect_for_each_screen(function(s)
             cpuwidget,
             ---batterywidget,
             --thermalwidget,
-            myweather.icon,
+            --myweather.icon,
             wibox.widget.systray(),
             mytextclock,
             s.mylayoutbox,
