edb7cda2a65d4a50fca5a96c949ddff820c2b8d2
hanez <you@hanez.org>
2024-04-07 03:16:14 +0200

    ACME update script cleaned up...

--- bin/ac.sh
+++ bin/ac.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#cat /etc/lighttpd/vhosts.d/systems.findeisen.conf.nossl > /etc/lighttpd/vhosts.d/systems.findeisen.conf
-
-/etc/init.d/lighttpd restart
-
 acme.sh --cron --force --home /root/.acme.sh
 
 for host in $(ls /etc/acme | tr '\n' ' '); do
@@ -12,15 +8,12 @@ for host in $(ls /etc/acme | tr '\n' ' '); do
     cat /etc/acme/$host/fullchain.cer >> /etc/acme/$host/$host.pem
 done
 
-#cat /etc/lighttpd/vhosts.d/systems.findeisen.conf.ssl > /etc/lighttpd/vhosts.d/systems.findeisen.conf
-
-cat /etc/acme/mail.xw3.org/fullchain.cer > /opt/mailcow-dockerized/data/assets/ssl/mail.xw3.org/cert.pem
-cat /etc/acme/mail.xw3.org/mail.xw3.org.key > /opt/mailcow-dockerized/data/assets/ssl/mail.xw3.org/key.pem
-
 /etc/init.d/lighttpd restart
 /etc/init.d/nginx restart
 /etc/init.d/prosody restart
 
+cat /etc/acme/mail.xw3.org/fullchain.cer > /opt/mailcow-dockerized/data/assets/ssl/mail.xw3.org/cert.pem
+cat /etc/acme/mail.xw3.org/mail.xw3.org.key > /opt/mailcow-dockerized/data/assets/ssl/mail.xw3.org/key.pem
 cd /opt/mailcow-dockerized
 docker-compose restart
 
