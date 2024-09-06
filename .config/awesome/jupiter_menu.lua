e9bbd1646492fa2fbddb9c9a435848d6aa43afec
hanez <you@hanez.org>
2024-09-06 03:32:27 +0200

    Menu update.

--- .config/awesome/jupiter_menu.lua
+++ .config/awesome/jupiter_menu.lua
@@ -159,6 +159,7 @@ internetmenu = {
     { "electrum", "/usr/bin/electrum", "/usr/share/icons/hicolor/128x128/apps/electrum.png" },
     { "firefox", "/usr/bin/firefox", "/usr/share/icons/hicolor/48x48/apps/firefox.png" },
     { "firefox (private)", "/usr/bin/firefox --private-window", "/usr/share/icons/hicolor/48x48/apps/firefox.png" },
+    { "gftp", "/usr/bin/gftp-gtk", "/usr/share/icons/hicolor/scalable/apps/gftp.svg" },
     { "liferea", "/usr/bin/liferea", "/usr/share/icons/hicolor/scalable/apps/net.sourceforge.liferea.svg" },
     { "mega", "/usr/bin/megasync", "/usr/share/icons/hicolor/48x48/apps/mega.png" },
     { "nextcloud", "/usr/bin/nextcloud", "/usr/share/icons/hicolor/48x48/apps/Nextcloud.png" },
