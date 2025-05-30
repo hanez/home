cf2aa776eb191d000670d0297c6e381abd8a7b8d
Johannes Findeisen <you@hanez.org>
2025-05-30 03:41:22 +0200

    Awesome config changes.

--- .config/awesome/tablet_taskbar.lua
+++ .config/awesome/tablet_taskbar.lua
@@ -45,7 +45,7 @@ awful.screen.connect_for_each_screen(function(s)
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
             --netwidget,
-            cpuwidget,
+            --cpuwidget,
             ---batterywidget,
             --thermalwidget,
             --myweather.icon,
